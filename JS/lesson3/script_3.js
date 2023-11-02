bb784024621dee54bd3d021df442dfceb3c9b6e3
oddi <mail@mail.com>
2023-11-02 19:29:19 +0300

    last

--- JS/lesson3/script_3.js
+++ JS/lesson3/script_3.js
@@ -1,35 +1,37 @@
-function annogram(str_1, str_2) {
+function isAnnogram(str_1, str_2) {
   let count_1 = 0
   let count_2 = 0
   let letter = ''
-  if (str_1.length === str_2.length) {
-    for (let i = 0; i < str_1.length; i++) {
-      if (str_2.indexOf(str_1[i]) === -1) {
-        return false
-      }
-    }
+  str_1 = str_1.toLowerCase()
+  str_2 = str_2.toLowerCase()
+  if (str_1.length !== str_2.length) return false 
     for (let item of str_1) {
       letter = item
       for (let j = 0; j < str_1.length; j++) {
         letter === str_1[j] ? (count_1 += 1) : null
         letter === str_2[j] ? (count_2 += 1) : null
+        if (count_2==0) return false
       }
       if (count_1-1 !== count_2-1) return false
     }
     return true
-  } else {
-    return false
   }
-}
 
-let a = annogram('finder', 'friend')
-console.log(a)
-a = annogram('test', 'sets')
-console.log(a)
-a = annogram('abc', 'aaa')
-console.log(a)
-a = annogram('abb', 'aab')
-console.log(a)
+  let a = isAnnogram('fiNder', 'frIenD')
+  console.log(a)
+  a = isAnnogram('test', 'sets')
+  console.log(a)
+  a = isAnnogram('abc', 'aaa')
+  console.log(a)
+  a = isAnnogram('abb', 'aab')
+  console.log(a)
+  
+
+// for (let i = 0; i < str_1.length; i++) {
+    //   if (str_2.indexOf(str_1[i]) === -1) {
+    //     return false
+    //   }
+    // }
 
 // let str_arr = str_2.split('')
 // for (let i=0; i<str_1.length;i++){
