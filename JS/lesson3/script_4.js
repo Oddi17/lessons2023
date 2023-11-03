95ba70a4743dc5c1da1856d0648bf5632f08474b
oddi <mail@mail.com>
2023-11-03 15:55:25 +0300

    lasteckaya

--- JS/lesson3/script_4.js
+++ JS/lesson3/script_4.js
@@ -6,8 +6,8 @@ function minmax(arr) {
   let max_num = arr[0]
   let pos_min = 0
   let pos_max = 0
-  arr.shift()
-  console.log(Math.max(arr))
+  // arr.shift()
+  // console.log(Math.max(arr))
   for (item of arr) {
     min_num > item ? (min_num = item) : null
     max_num < item ? (max_num = item) : null
@@ -19,7 +19,9 @@ function minmax(arr) {
   return arr
 }
 
-let arr = [24, 12, 22, 11, 5, 4, 3, 6, 28, 56]
+// let arr = [24, 12, 22, 11, 5, 4, 3, 6, 28, 56]
+let arr = [1,2,3,4,5,6,7]
+
 let a = minmax(arr)
 
 console.log(a)
