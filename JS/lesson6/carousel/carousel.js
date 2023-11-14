ac00d107b9874f6cceb073249194c61ea16176b1
oddi <mail@mail.com>
2023-11-14 14:15:43 +0300

    carousel

--- JS/lesson6/carousel/carousel.js
+++ JS/lesson6/carousel/carousel.js
@@ -1,90 +1,91 @@
-// const $controlElements = document.querySelectorAll('.controls')
-// const $carouselContainer = document.querySelector('.carousel-container')
-// const $indicatorsContainer = document.querySelector('.carousel-indicators')
-// let itemWidth = $carouselContainer.offsetWidth
-// let translate = 0
-// let loop = 1
-// let imgQuantity = $carouselContainer.children.length
-// let currentImg = 0
+const images = ['img1.jpg', 'img2.jpg', 'img3.jpg']
 
-let updateIndicators = () => {
-  let arrFromCollection = Array.from($indicatorsContainer.children)
-  arrFromCollection.forEach((elem, index) => {
-    // elem.className = 'dot'
-    // if (index === currentImg) {
-    //   elem.className = 'dot active'
-    // }
-    elem.className = 'dot ' + (index === currentImg ? 'active' : '')
+let createCarousel = (elemid, images) => {
+  const $mainCountainer = document.getElementById(elemid)
+
+  // создание carousel
+  const $carouselContainer = document.createElement('div')
+  $carouselContainer.className = 'carousel-container'
+  for (let item of images) {
+    let $imgContainer = document.createElement('div')
+    $imgContainer.className = 'carousel-item'
+    $imgContainer.innerHTML = '<img src="' + item + '" alt="">'
+    $carouselContainer.append($imgContainer)
+  }
+
+  //создание контролов
+  const $controlElements = document.createElement('div')
+  $controlElements.className = 'carousel-controls'
+  $controlElements.innerHTML = ` 
+      <div class="controls prev"><</div>
+      <div class="controls next">></div>
+  `
+
+  //создание индикаторов
+  const $indicatorsContainer = document.createElement('div')
+  $indicatorsContainer.className = 'carousel-indicators'
+  $indicatorsContainer.innerHTML = `
+      <div class="dot active"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+  `
+
+  //добавление в html файл
+  $mainCountainer.append($carouselContainer)
+  $mainCountainer.append($controlElements)
+  $mainCountainer.append($indicatorsContainer)
+
+  let itemWidth = $carouselContainer.offsetWidth
+  let translate = 0
+  let loop = 1
+  let imgQuantity = $carouselContainer.children.length
+  let currentImg = 0
+
+  let updateIndicators = () => {
+    let arrFromCollection = Array.from($indicatorsContainer.children)
+    arrFromCollection.forEach((elem, index) => {
+      // elem.className = 'dot'
+      // if (index === currentImg) {
+      //   elem.className = 'dot active'
+      // }
+      elem.className = 'dot ' + (index === currentImg ? 'active' : '')
+    })
+  }
+
+  let controlsClickHandler = (e) => {
+    // console.dir(e.target.classList.contains('prev'))
+    //Зацикливание
+
+    if (e.target.classList.contains('prev')) {
+      if (translate !== 0) {
+        translate += itemWidth
+      } else {
+        if (loop) translate = (imgQuantity - 1) * itemWidth * -1
+      }
+    } else if (e.target.classList.contains('next')) {
+      //перемотка
+      if ((imgQuantity - 1) * itemWidth !== -translate) {
+        translate -= itemWidth
+      } else {
+        if (loop) translate = 0
+      }
+    }
+
+    currentImg = Math.abs(translate / itemWidth)
+
+    // console.log('index' + currentImg)
+    updateIndicators()
+
+    $carouselContainer.style.transform = 'translateX(' + translate + 'px)'
+    //что делаем, когда получили клик
+  }
+
+  let arrFromControls = Array.from($controlElements.children)
+  // console.log(arrFromControls,typeof(arrFromControls))
+  arrFromControls.forEach((element) => {
+    //тут происходят итерации цикла. перебор controlElements
+    element.addEventListener('click', controlsClickHandler)
   })
 }
 
-let controlsClickHandler = (e) => {
-  console.dir(e.target.classList.contains('prev'))
-
-  //Зацикливание
-
-  if (e.target.classList.contains('prev')) {
-    if (translate !== 0) {
-      translate += itemWidth
-    } else {
-      if (loop) translate = (imgQuantity - 1) * itemWidth * -1
-    }
-  } else if (e.target.classList.contains('next')) {
-    //перемотка
-    if ((imgQuantity - 1) * itemWidth !== -translate) {
-      translate -= itemWidth
-    } else {
-      if (loop) translate = 0
-    }
-  }
-
-  currentImg = Math.abs(translate / itemWidth)
-
-  console.log('index' + currentImg)
-  updateIndicators()
-
-  $carouselContainer.style.transform = 'translateX(' + translate + 'px)'
-  //что делаем, когда получили клик
-}
-
-// $controlElements.forEach((element) => {
-//   //тут происходят итерации цикла. перебор controlElements
-//   element.addEventListener('click', controlsClickHandler)
-// })
-
-let createCarousel = (elemid, counter, images) => {
-  // carousel
-
-  const $mainCountainer = document.getElementById(elemid)
-
-  let template = `
-    <div class="carousel-container">
-        <div class="carousel-item">
-            <img src="img1.jpg" alt="">
-        </div>
-        <div class="carousel-item">
-            <img src="img2.jpg" alt="">
-        </div>
-        <div class="carousel-item">
-            <img src="img3.jpg" alt="">
-        </div>
-    </div>
-    <div class="carousel-controls"> 
-        <div class="controls prev"><</div>
-        <div class="controls next">></div>
-    </div>
-    <div class="carousel-indicators">
-        <div class="dot active"></div>
-        <div class="dot"></div>
-        <div class="dot"></div>
-    </div> 
-    `
-
-    $mainCountainer.innerHTML = template
-
-    // $mainCountainer.append(template)
-
-
-}
-
-createCarousel('carousel_1')
+createCarousel('carousel_1', images)
--- JS/lesson6/old lesson6/carousel.js
+++ JS/lesson6/old lesson6/carousel.js
@@ -1,18 +1,18 @@
 const $controlElements = document.querySelectorAll('.controls')
 const $carouselContainer = document.querySelector('.carousel-container')
-let itemWidth = -1 * $carouselContainer.parentElement.offsetWidth
+let itemWidth = $carouselContainer.offsetWidth
 
 let controlsClickHandler = (e) => {
   //   console.dir(e.target.classList.contains('prev'))
   let translate = 0
   if (e.target.classList.contains('prev')) {
-    translate -= itemWidth
+    translate += itemWidth
     // if (translate < 0) {
 
     // }
   } else if (e.target.classList.contains('next')) {
     // $carouselContainer.style.transform = 'translateX(' + translate + 'px)'
-    translate += itemWidth
+    translate -= itemWidth
     // if (translate > translate * 2) {
     //   translate += itemWidth
     // }
