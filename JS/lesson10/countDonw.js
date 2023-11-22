4acfcea6d7655310b33f18050d44944d6ed405d7
Першин Владислав Алексеевич <pershin@alliance-electro.ru>
2023-11-22 16:59:43 +0300

    timer

--- JS/lesson10/countDonw.js
+++ JS/lesson10/countDonw.js
@@ -11,11 +11,16 @@ const Timer = () => {
 
   //   console.log(timeDiff)
   let days = Math.floor(timeDiff / 1000 / 3600 / 24)
-  let hours = Math.floor(timeDiff / 1000 / 3600) - Math.floor(timeDiff / 1000 / 3600 / 24) * 24
+  let hours =
+    Math.floor(timeDiff / 1000 / 3600) -
+    Math.floor(timeDiff / 1000 / 3600 / 24) * 24
   // let hours = Math.floor(timeDiff / 1000 / 60 / 60)
   let minutes = Math.floor((timeDiff / 1000 / 60) % 60)
   let seconds = 60 - currentTime.getSeconds()
 
+  // minutes = 1
+  // hours = 0
+  // days = 1
   // console.log('Days:'+days)
   // console.log('Hours:'+hours)
   // console.log('Minutes:' +minutes)
@@ -33,29 +38,36 @@ const Timer = () => {
 
   //интервальная функция
   setInterval(() => {
-    if (days === hours === minutes === 0) {
+    if (seconds === 0 && days === 0 && hours === 0 && minutes === 0) {
       seconds = 0
       console.log('End of timer')
       return
     }
-    if (seconds === 0) seconds = 60
-    $secondsTime.innerHTML = --seconds
-    
+    --seconds
+    $secondsTime.innerHTML = seconds
 
-    if (minutes === 0 && hours > 0) minutes = 60
-    if (seconds === 0) $minutesTime.innerHTML = --minutes
-    
+    if (hours === 0 && days > 0 && minutes === 0 && seconds === 0) {
+      hours = 24
+      --days
+    }
+    $hoursTime.innerHTML = hours
+    $daysTime.innerHTML = days
 
-    if (hours === 0 && days > 0) hours = 24
-    if (minutes === 0) $hoursTime.innerHTML = --hours
-    
+    if (minutes === 0 && hours > 0 && seconds === 0) {
+      minutes = 60
+      --hours
+    }
+    $hoursTime.innerHTML = hours
+    $minutesTime.innerHTML = minutes
 
-    if (hours === 0) $daysTime.innerHTML = --days
-
-    
+    if (seconds === 0 && minutes > 0) {
+      seconds = 60
+      --minutes
+      $secondsTime.innerHTML = seconds
+      $minutesTime.innerHTML = minutes
+    }
   }, 1)
 }
-
 
 const $formDate = document.querySelector('.clock-input')
 $formDate.addEventListener('change', (event) => {
