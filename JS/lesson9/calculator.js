838745dacccafb414d113f74ec2af69a2c4ab725
Першин Владислав Алексеевич <pershin@alliance-electro.ru>
2023-11-30 17:21:01 +0300

    calculator

---
+++ JS/lesson9/calculator.js
@@ -0,0 +1,18 @@
+
+
+const $numbersOftable = document.querySelectorAll('.row')
+console.log($numbersOftable)
+
+
+
+$numbersOftable.forEach((elem,i) => {
+    console.log(elem)
+    elem.forEach((element)=>{
+        let symbol = element.querySelector('span') 
+        console.log(element)
+        symbol.addEventListener
+        
+
+    })
+
+})
