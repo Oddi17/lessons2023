ffd527c5f5ffa71fea282db38ff32637b631c57a
Першин Владислав Алексеевич <pershin@alliance-electro.ru>
2023-12-01 16:37:22 +0300

    calc

--- JS/lesson9/calculator.js
+++ JS/lesson9/calculator.js
@@ -1,18 +1,84 @@
+const evaluateValue = () =>{
+    result = math.evaluate(str_of_operation)
+    console.log(result,typeof(result))
+    $windowCalculator.innerHTML += '='+result
+    //$windowCalculatorResult = document.querySelector('.input #result')
+    //$windowCalculatorResult.innerHTML = '=' + ' ' + result
+    
+}
 
 
-const $numbersOftable = document.querySelectorAll('.row')
-console.log($numbersOftable)
+const aligningSize = () => {
+    
+}
+
+const currentValues = (event) => {
+    
+    symbolclass = event.target.parentElement.classList[0]
+    console.log("last:",symbolclass)
+    symbol = event.target.firstChild.data
+    console.log("last:",last_symbol_class)
+    console.log("input_symbol:",symbolclass)
 
 
+    if ((perem) && (((symbolclass === 'number') && (symbol !=='AC')) || (symbol === '='))){
+        return
+    }else{
+        perem = false
+    }
 
+    if (str_of_operation.length === 0 && symbolclass === 'symbol') {
+        $windowCalculator.innerHTML = symbol
+        return
+    }
+    
+    if (symbolclass === 'symbol'){
+        if ((last_symbol_class) && (symbolclass === last_symbol_class )) {
+            // console.log('two symbols podryad')
+            // console.log(str_of_operation)
+            return
+        }
+    }
+
+    if (symbol === '÷') {
+        str_of_operation += '/'
+    }else if (symbol === 'AC') {
+        str_of_operation = ''
+        $windowCalculator.innerHTML = start_value
+        return
+    }else if (symbol=== '='){
+        //evaluateValue()
+        result = math.evaluate(str_of_operation)
+        $windowCalculator.innerHTML += '='+result
+        str_of_operation = result
+        perem = true
+        return
+    }
+    else{
+        str_of_operation += symbol
+    }   
+    $windowCalculator.innerHTML = str_of_operation
+    last_symbol_class = symbolclass
+    
+}
+
+const currentValue = () => {
+
+}
+
+const $numbersOftable = document.querySelectorAll('.row span')
+$windowCalculator =  document.querySelector('.input #current')
+start_value = '0'
+$windowCalculator.innerHTML = start_value
+let perem = false
+let str_of_operation = ''
+let last_symbol_class
 $numbersOftable.forEach((elem,i) => {
-    console.log(elem)
-    elem.forEach((element)=>{
-        let symbol = element.querySelector('span') 
-        console.log(element)
-        symbol.addEventListener
-        
+    new Array(elem).forEach((element)=>{
+        element.addEventListener('click', (event)=>{
+            currentValues(event)
 
+        })
     })
 
 })
