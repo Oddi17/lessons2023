3e0437b3a32290b500f48f61550e30aae91d4074
Pershin <mail@mail.com>
2024-01-12 17:18:31 +0300

    project

--- my_project/react-order-project/src/App.jsx
+++ my_project/react-order-project/src/App.jsx
@@ -6,16 +6,14 @@ import './Calendar_another.css'
 import CalendarMy from './calendar/Calendar_my'
 import '../src/calendar/CalendarMy.scss'
 
-
-
-
 export default function App() {
   const dateNow = new Date()
   const nameMonthNow = dateNow.toLocaleString('ru', {
     month: 'long',
   })
-  const upperNameMonthNow = nameMonthNow.charAt(0).toUpperCase() + nameMonthNow.slice(1)
- 
+  const upperNameMonthNow =
+    nameMonthNow.charAt(0).toUpperCase() + nameMonthNow.slice(1)
+
   return (
     <div className="container">
       <header>
@@ -29,13 +27,18 @@ export default function App() {
         <h1>Добро пожаловать!</h1>
         <section id="1">
           <div id="collapse_1">
-            Забронировать столик
-              {/*<
-               <Calendar 
-              onChange = {onChangeCalendar}
-              value = {calendarValue}
-               /> */}
-              <CalendarMy dateNow={dateNow} name={upperNameMonthNow} />
+            <h2>
+              <button aria-expanded="false">Забронировать столик</button>
+            </h2>
+            <div class="collapse_elem">
+              Some TEXT!
+              <svg viewBox="" aria-hidden="true" focusable="false">
+                <rect class="vert" height="8" width="2" y="1" x="4" />
+                <rect height="2" width="8" y="4" x="1" />
+              </svg>
+            </div>
+
+            <CalendarMy dateNow={dateNow} name={upperNameMonthNow} />
           </div>
           <div id="collapse_2">Меню</div>
           <div id="collapse_3">Фото</div>
