9f50baddba1056fca1126b320bd4a6f46a2d36c7
oddi <mail@mail.com>
2024-01-28 22:40:09 +0300

    666

--- my_project/react-order-project/src/App.jsx
+++ my_project/react-order-project/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './css/authorization.css'
 import { useState } from 'react'
 import CalendarMy from './components/calendar/Calendar_my'
 import './components/calendar/CalendarMy.scss'
@@ -17,23 +18,25 @@ export default function App() {
 
   return (
     <div className="container">
-        <header>
-            <h2>ST.Restaurant</h2>
-            <div className="nav">
-              <div>Мероприятия</div>
-              <div>О нас</div>
-              <div>Отзывы</div>
-            </div>
-            <div className="icons">
-              <span class="material-symbols-outlined">mail</span>
-              <span class="material-symbols-outlined">call</span>
-              <img src={telegram}></img>
-              <img src={instagram}></img>
-              <i class="bi bi-instagram"></i>
-            </div>
-        </header>
+      <header>
+        <h2>ST.Restaurant</h2>
+        <div className="nav">
+          <div>Мероприятия</div>
+          <div>О нас</div>
+          <div>Отзывы</div>
+          <div>Войти</div>
+        </div>
+        <div className="icons">
+          <span class="material-symbols-outlined">mail</span>
+          <span class="material-symbols-outlined">call</span>
+          <img src={telegram}></img>
+          <img src={instagram}></img>
+          <i class="bi bi-instagram"></i>
+        </div>
+      </header>
       <div className="main_container">
-          <section id="1">
+        <div className="wallpaper">
+          <section id="first">
             {/* <Collapsible label="Introduction">
               <h1>introduction</h1>
               <p>
@@ -41,20 +44,34 @@ export default function App() {
                 block enabling users to expand or collapse to access its details.
               </p>
             </Collapsible> */}
-            <Collapsible label="Забронировать" classIn="collapse 1">
+            <Collapsible label="Забронировать" classIn="collapse first">
               <CalendarMy dateNow={dateNow} name={upperNameMonthNow} />
             </Collapsible>
             {/* <div>
               <CalendarMy dateNow={dateNow} name={upperNameMonthNow} />
             </div> */}
-            <Collapsible label="Меню" classIn="collapse 2">
+            <Collapsible label="Меню" classIn="collapse second">
               <div>КАКОЙ-ТО ТЕКСТ</div>
+              <CalendarMy dateNow={dateNow} name={upperNameMonthNow} />
             </Collapsible>
-            <Collapsible label="Фото" classIn="collapse 3">
+            <Collapsible label="Фото" classIn="collapse third">
               <div>РАЗНЫЕ ФОТКИ</div>
             </Collapsible>
           </section>
-      </div>
+          </div>
+          <section id="second">
+            <div>ЧТО-ТО! под номером 2</div>
+          </section>
+          <section id="third">
+            <div>ЧТО-ТО! под номером 3</div>
+          </section>
+          {/*В общем это для модалки ЛОГИНА */}
+          <div className='fade hidden'>
+            <div className='modal'>
+              Модалка
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
